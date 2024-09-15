47448fe4e220c4582969e0deddaee35c0d1a2ffe
dariagritsienko <dariagritsienko@yandex.ru>
2024-09-15 21:36:44 +0300

    try fix

--- todo-client/vite.config.js
+++ todo-client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/todo-list-react/todo-client/', // Путь до приложения
+    base: '',
     server: {
         open: true,
         port: 3000,
