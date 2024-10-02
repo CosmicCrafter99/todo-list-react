f2bee5be19ad2bdc349a1e157dcc30bf8a7dc693
dariagritsienko <dariagritsienko@yandex.ru>
2024-10-03 00:17:09 +0300

    try to fix port

--- todo-client/vite.config.js
+++ todo-client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     base: '',
+    preview: {
+        port: process.env.PORT || 3000,
+    },
     server: {
         open: true,
         port: 3000,
